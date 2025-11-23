b10798204ea022604cb8268e07748936fba6c856
Zhassulan Baigozha <Zhassulan.Baigozha@nitec.kz>
2025-11-23 22:30:35 +0500

    fix: fastify

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import Fastify from "fastify";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = Fastify({ logger: true });
+
+app.get("/", async () => ({ status: "Hello Word" }));
+
+app.listen({ port: Number(process.env.PORT) || 8080 }, (err) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+});
