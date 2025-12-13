30dac643c5c9af469240a8a5eb5a7dd91bf97531
Zhassulan Baigozha <Zhassulan.Baigozha@nitec.kz>
2025-12-13 11:02:27 +0500

    fix: pool

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 // src/index.ts
 import dotenv from "dotenv";
 import express from "express";
+import { pool } from "./db";
 
 dotenv.config();
 
 const app = express();
 
+const port = Number(process.env.PORT) || 8080;
+
 app.get("/", (_req, res) => {
 	res.json({ status: "Hello World" });
 });
 
-const port = Number(process.env.PORT) || 8080;
+app.get("/health", async (_req, res) => {
+	const { rows } = await pool.query("SELECT 1");
+	res.json({ ok: true, db: rows[0] });
+});
 
 app.listen(port, "0.0.0.0", () => {
 	console.log(`Server is running on port ${port}`);
